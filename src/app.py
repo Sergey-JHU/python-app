2b47c862fd4b0449daeaa208262f208923023a2c
Sergey <skravec1@jh.edu>
2025-06-19 15:31:17 -0400

    first commit

---
+++ src/app.py
@@ -0,0 +1,27 @@
+
+from flask import Flask, jsonify
+import datetime
+import socket
+
+app = Flask(__name__)
+
+@app.route('/api/v1/details')
+
+def details():
+    #return '<h1>Hello World</h1>'
+    return jsonify({'time': datetime.datetime.now().strftime("%I:%M:%S%p on %B %d, %Y"),
+                    'hostname': socket.gethostname()
+                  })
+
+@app.route('/api/v1/health')
+
+def health():
+    #return '<h1>Hello World</h1>'
+    return jsonify({'status': 'up'}), 200
+
+if __name__ == '__main__':
+
+    app.run(host="0.0.0.0")
+
+#'api/v1/details'
+#'api/v1/health'
