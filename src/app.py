d3eef203975f5eb46ab5ae6fd8c49fe05801d1c3
Sergey <skravec1@jh.edu>
2025-07-14 16:43:32 -0400

    test5

--- src/app.py
+++ src/app.py
@@ -11,7 +11,7 @@ def details():
     #return '<h1>Hello World</h1>'
     return jsonify({'time': datetime.datetime.now().strftime("%I:%M:%S%p on %B %d, %Y"),
                     'hostname': socket.gethostname(),
-                    'message': 'Hi CWS Team, You are doing great!!'
+                    'message': 'Hi CWS Team, You are doing great!!!'
                   })
 
 @app.route('/api/v1/health')
